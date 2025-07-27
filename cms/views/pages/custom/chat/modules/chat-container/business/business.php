4e5047c8b88adb01d72e14eee134e626998f4997
heavymetalmixer <heavymetalmixer@gmail.com>
2025-07-27 18:17:42 -0500

    More working on the Whatsapp CMS GUI

--- cms/views/pages/custom/chat/modules/chat-container/business/business.php
+++ cms/views/pages/custom/chat/modules/chat-container/business/business.php
@@ -1,6 +1,6 @@
 <div class="msg bot">
-	<?php echo $value->business_message ?> <br>
-	<span class="small text-muted float-end mt-2">
-		<?php echo TemplateController::formatDate(6,$value->date_updated_message) ?>		
-	</span>
+  <?php echo $value->business_message; ?> <br>
+  <span class="small text-muted float-end mt-2">
+    <?php echo TemplateController::formatDate(6, $value->date_updated_message); ?>
+  </span>
 </div>
--- cms/views/pages/custom/chat/modules/chat-container/chat-container.php
+++ cms/views/pages/custom/chat/modules/chat-container/chat-container.php
@@ -1,108 +1,68 @@
 <div class="chat-container">
-
   <div class="chat-header d-flex justify-content-between">
 
     <?php if (!empty($contacts)): ?>
 
-      <!--======================================
-      Validar Asistente IA
-      ========================================-->
-
       <div class="bot-name bg-white rounded p-1" id="botName">
         <div class="custom-control custom-checkbox">
-          <input 
-          type="checkbox" 
-          class="custom-control-input mt-2 form-check-input" 
-          id="customCheck" 
-          name="example1" 
-          <?php if (isset($_GET["phone"]) && explode("_",$_GET["phone"])[1] == 1 || $contacts[0]->ai_contact == 1): ?> checked <?php endif ?>
+          <input type="checkbox"
+          class="custom-control-input mt-2 form-check-input"
+          id="customCheck"
+          name="example1"
+
+          <?php if ($contacts[0]->ai_contact == 1): ?> checked
+          <?php endif ?>
+
           style="width:18px !important; height:18px !important">
           <label class="custom-control-label mb-1 me-1" for="customCheck"><i class="fas fa-robot text-success"></i></label>
-        </div> 
+        </div>
 
-      </div> 
-
-      <!--======================================
-      Número de teléfono
-      ========================================-->
-
-      <div class="text-center">
-        <?php if (isset($_GET["phone"])): $phoneContact = explode("_",$_GET["phone"])[0] ?>
-
-          <?php else: $phoneContact = $contacts[0]->phone_contact ?>
-          
-        <?php endif ?>
-
-          +<?php echo mb_substr($phoneContact,0,2) ?> 
-           <?php echo mb_substr($phoneContact,2,3) ?>
-           <?php echo mb_substr($phoneContact,5,7) ?>  
       </div>
-
-      <!--======================================
-      Gestión de Contacto
-      ========================================-->
-
+      <div class="text-center">
+        +<?php echo mb_substr($contacts[0]->phone_contact, 0, 2); ?>
+        <?php echo mb_substr($contacts[0]->phone_contact, 2, 3); ?>
+        <?php echo mb_substr($contacts[0]->phone_contact, 5, 7); ?>
+      </div>
       <div>
         <button type="button" class="btn btn-sm text-white rounded m-0 px-1 py-0 border-0"><i class="bi bi-pencil-square"></i></button>
         <button type="button" class="btn btn-sm text-white rounded m-0 px-1 py-0 border-0"><i class="bi bi-trash"></i></button>
       </div>
-      
+
     <?php endif ?>
 
   </div>
 
   <div class="chat-body" id="chatBody">
 
-    <?php if (!empty($messages)): $messages = array_reverse($messages) ?>
-
-      <input type="hidden" id="phoneMessage" value="<?php echo $phoneContact ?>">
-      <input type="hidden" id="orderMessage" value="<?php echo end($messages)->order_message ?>">
-
-      <!--======================================
-      Fecha de los mensajes
-      ========================================-->
+    <?php if (!empty($messages)): $messages =array_reverse($messages) ?>
 
       <div class="d-flex justify-content-center text-center">
-        
         <span class="badge border bg-success rounded py-2 px-2">
 
-          <?php if (date("Y-m-d") == TemplateController::formatDate(8,end($messages)->date_updated_message)): ?>
+          <?php if (date("Y-m-d") == TemplateController::formatDate(8, end($messages)->date_updated_message)): ?>
             Hoy
           <?php else: ?>
-            <?php echo TemplateController::formatDate(7,end($messages)->date_updated_message) ?>
+            <?php echo TemplateController::formatDate(7, end($messages)->date_updated_message); ?>
           <?php endif ?>
 
         </span>
-
       </div>
-
-      <!--======================================
-      Separar conversación del cliente con el negocio
-      ========================================-->
 
       <?php foreach ($messages as $key => $value): ?>
 
         <?php if ($value->type_message == "business"): ?>
-
           <?php include "business/business.php"; ?>
-          
         <?php endif ?>
 
         <?php if ($value->type_message == "client"): ?>
-
           <?php include "client/client.php"; ?>
-          
         <?php endif ?>
-        
+
       <?php endforeach ?>
-      
+
     <?php endif ?>
 
   </div>
-
-  <!--======================================
-  Formulario para enviar mensajes manuales
-  ========================================-->
 
   <div class="chat-footer position-relative w-100">
 
