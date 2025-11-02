a0e83dd3cf401da3e8f38851b1ef96cadcde14b9
heavymetalmixer <heavymetalmixer@gmail.com>
2025-11-02 16:48:59 -0500

    Mercado Pago payment button now appears

--- cms/webhook/mercado_pago/index.php
+++ cms/webhook/mercado_pago/index.php
@@ -11,57 +11,185 @@ $test_env->load();
 
 use MercadoPago\MercadoPagoConfig;
 use MercadoPago\Client\Preference\PreferenceClient;
+// use MercadoPago\Exceptions\MPApiException;
+// use MercadoPago\Resources\Preference;
 
-$access_token = $_ENV['MERCADOPAGO_ACCESS_TOKEN'];
-MercadoPagoConfig::setAccessToken(".$access_token.");
+// function authenticate() {
+
+//     // Autenticación
+//     $access_token = $_ENV['MERCADOPAGO_ACCESS_TOKEN'];
+//     MercadoPagoConfig::setAccessToken(".$access_token.");
+//     MercadoPagoConfig::setRuntimeEnviroment(MercadoPagoConfig::LOCAL);
+// }
+
+// function create_preference_request($items, $payer) : array {
+
+//     $payment_methods = [
+//         "excluded_payment_methods" => [],
+//         "installments" => 12,
+//         "default_installments" => 1
+//     ];
+
+//     $back_urls = array(
+//         "success" => "mercadopago.success",
+//         "failure" => "mercadopago.failed"
+//     );
+
+//     $request = array(
+//         "items" => $items,
+//         "payer" => $payer,
+//         "payment_methods" => $payment_methods,
+//         "back_urls" => $back_urls,
+//         "statement_descriptor" => "NAME_DISPLAYED_IN_USER_BILLING",
+//         "external_reference" => "1234567890",
+//         "expires" => false,
+//         "auto_return" => "approved"
+//     );
+
+//     return $request;
+// }
+
+// function create_payment_preference() : ?Preference {
+
+//     // Datos de los productos a comprar
+//     $product1 = array(
+//         "id" => "12345674980",
+//         "title" => "Product 1 Title",
+//         "description" => "Product 1 Description",
+//         "currency_id" => "COP",
+//         "quantity" => 12,
+//         "unit_price" => 9.90
+//     );
+
+//     $product2 = array(
+//         "id" => "9012345678",
+//         "title" => "Product 2 Title",
+//         "description" => "Product 2 Description",
+//         "currency_id" => "COP",
+//         "quantity" => 5,
+//         "unit_price" => 19.90
+//     );
+
+//     $items = array($product1, $product2);
+
+//     // Se adquieren los datos del comprador (función propia)
+//     // $user = getSessionUser();
+
+//     // $payer = array(
+//     //     "name" => $user->name,
+//     //     "surname" => $user->surname,
+//     //     "email" => $user->email,
+//     // );
+
+//     $payer = array(
+//         "name" => "John",
+//         "surname" => "Caro",
+//         "email" => "asdfiunsjdfn@gmail.com"
+//     );
+
+//     // Se crea el objecto de tipo request para ser enviado a la API de Mercado Pago
+//     $request = create_preference_request($items, $payer);
+
+//     $client = new PreferenceClient();
+
+//     try {
+
+//         // Se envía el request a la API
+//         $preference = $client->create($request);
+//         return $preference;
+//     }
+//     catch(MPApiException $error) {
+
+//         echo '<pre>$error: '; print_r($error); echo '</pre>';
+//         return null;
+//     }
+// }
+
+// $preference = create_payment_preference();
+
+// // $client = new PreferenceClient();
+
+// // $preference = $client->create([
+
+// //     "items" => array(
+// //         array(
+// //             "id" => "DEP-0001",
+// //             "title" => "Balon de futbol",
+// //             "quantity" => 1,
+// //             "unit_price" => 550
+// //         )
+// //     ),
+
+// //     "statement_descriptor" => "Mi tienda de Prueba",
+// //     "external_reference" => "MTP001"
+// // ]);
+
+// // echo implode($preference);
+// // echo '<pre>$preference '; print_r($preference); echo '</pre>';
+
+$test_access_token = $_ENV['MERCADOPAGO_TEST_ACCESS_TOKEN'];
+$public_key = $_ENV['MERCADOPAGO_PUBLIC_KEY'];
+
+MercadoPagoConfig::setAccessToken($test_access_token);
+MercadoPagoConfig::setRuntimeEnviroment(MercadoPagoConfig::LOCAL);
 
 $client = new PreferenceClient();
-
 $preference = $client->create([
-
+    "back_urls" => array(
+        "success" => "https://test.com/success",
+        "failure" => "https://test.com/failure",
+        "pending" => "https://test.com/pending"
+    ),
+    "auto_return" => "approved",
     "items" => array(
         array(
-            "id" => "DEP-0001",
-            "title" => "Balon de futbol",
+            "id" => "0001",
+            "title" => "Mi producto",
             "quantity" => 1,
-            "unit_price" => 550
+            "unit_price" => 2000
         )
-    ),
-
-    "statement_descriptor" => "Mi tienda de Prueba",
-    "external_reference" => "MTP001"
+    )
 ]);
 
-// echo implode($preference);
+// echo $preference;
 // echo '<pre>$preference '; print_r($preference); echo '</pre>';
 
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8"/>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-    <title>Document</title>
+    <title>Mi Integración con Checkout Pro</title>
 </head>
 <body>
-    <div id="wallet_container"></div>
+
+    <!-- Contenido de tu página -->
+
+
     <script src="https://sdk.mercadopago.com/js/v2"></script>
+
     <script>
-        const mp = new MercadoPago('APP_USR-2db32874-0cdd-40e1-98cc-57d677387844', {
+        // Tu código JavaScript irá aquí
 
-            locale: "es-CO"
-        });
+        const publicKey = "<?php echo $public_key; ?>";
+        const preferenceId = "<?php echo $preference->id; ?>";
 
-        const bricks_builder = mp.bricks();
-        const render_wallet_brick = async (bricks_builder) => {
+        // Inicializa el SDK de Mercado Pago
+        const mp = new MercadoPago(publicKey);
 
-            await bricks_builder.create("wallet", "walletBrick_container", {
-                initialization: { preferenceId: '<?php echo $preference->id; ?>', }
+        const bricksBuilder = mp.bricks();
+
+        const renderWalletBrick = async (bricksBuilder) => {
+            await bricksBuilder.create("wallet", "walletBrick_container", {
+                initialization: {
+                    preferenceId: "<PREFERENCE_ID>",
+                }
             });
         };
 
-        render_wallet_brick(bricks_builder);
+        renderWalletBrick(bricksBuilder);
     </script>
+    <div id="walletBrick_container"></div>
+
 </body>
 </html>
---
+++ cms/webhook/mercado_pago/success.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SUCCESS PAGE</title>
+</head>
+<body>
+    <p>The payment was completed successfully!</p>
+</body>
+</html>
